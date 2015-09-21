a4eb3670ec26e8389cd5a7c926be849cd81fa002
Max Stemplevski <smt@joomlalms.com>
2015-09-21 15:09:34 +0300

    Filters and sorting

--- lesson2/app/js/controllers.js
+++ lesson2/app/js/controllers.js
@@ -10,16 +10,49 @@ phonecatApp.controller( 'PhoneListCtrl', function ($scope)
     $scope.phones =
         [
             {'name': 'Nexus S',
-                'snippet': 'Fast just got faster with Nexus S.'},
+                'snippet': 'Fast just got faster with Nexus S.',
+                'status': true
+            },
             {'name': 'Motorola XOOM™ with Wi-Fi',
-                'snippet': 'The Next, Next Generation tablet.'},
+                'snippet': 'The Next, Next Generation tablet.',
+                'status': false
+            },
             {'name': 'MOTOROLA XOOM™',
-                'snippet': 'The Next, Next Generation tablet.'}
+                'snippet': 'The Next, Next Generation tablet.',
+                'status': true}
         ]
 
     //Filter
     var date = new Date();
     $scope.today = date;
 
+    $scope.doneAndFilter = function(phoneItem)
+    {
+        return phoneItem.name && phoneItem.status === true;
+    }
+
+    $scope.sortField = undefined; //"name";
+    $scope.reverse = false;
+
+    $scope.sort = function(fieldname)
+    {
+        if ($scope.sortField === fieldname)
+        {
+            $scope.reverse = !$scope.reverse;
+        }else
+        {
+            $scope.sortField = fieldname;
+            $scope.reverse = false;
+        }
+    }
+
+    $scope.isSortUp = function(fieldName)
+    {
+        return $scope.sortField === fieldName && !$scope.reverse;
+    }
+    $scope.isSortDown = function(fieldName)
+    {
+        return $scope.sortField === fieldName && $scope.reverse;
+    }
 }
 );
