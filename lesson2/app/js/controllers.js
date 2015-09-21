46a4751ee01bf8f8f2884d77a78f7af537aeb234
Max Stemplevski <smt@joomlalms.com>
2015-09-21 12:29:01 +0300

    Lesson 2 NG-REPEAT

--- lesson2/app/js/controllers.js
+++ lesson2/app/js/controllers.js
@@ -3,9 +3,18 @@
 /* Controllers */
 
 var phonecatApp = angular.module('phonecatApp', []);
-phonecatApp.controller('DemoCtrl',
-    function ($scope)
-    {
-        $scope.name = 'AngularJS Student';
-    }
+
+phonecatApp.controller( 'PhoneListCtrl', function ($scope)
+{
+    $scope.title = "Phones";
+    $scope.phones =
+        [
+            {'name': 'Nexus S',
+                'snippet': 'Fast just got faster with Nexus S.'},
+            {'name': 'Motorola XOOM™ with Wi-Fi',
+                'snippet': 'The Next, Next Generation tablet.'},
+            {'name': 'MOTOROLA XOOM™',
+                'snippet': 'The Next, Next Generation tablet.'}
+        ]
+}
 );
