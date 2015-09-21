ae1b06236c80fcbd08772a093bf9dfb6744db8dc
Max Stemplevski <smt@joomlalms.com>
2015-09-21 11:40:44 +0300

    Lesson 2 move and include controllers.js

--- lesson2/app/js/controllers.js
+++ lesson2/app/js/controllers.js
@@ -1,3 +1,11 @@
 'use strict';
 
 /* Controllers */
+
+var phonecatApp = angular.module('phonecatApp', []);
+phonecatApp.controller('DemoCtrl',
+    function ($scope)
+    {
+        $scope.name = 'AngularJS Student';
+    }
+);
